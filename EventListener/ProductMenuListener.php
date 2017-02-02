2ea165ac6b00f96791a86a72e335f8dd6881dbeb
pmdc <pmdavidcaron@gmail.com>
2017-02-02 13:33:49 -0500

    Page liste de cats de produits

---
+++ EventListener/ProductMenuListener.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: pmdc
+ * Date: 24/01/17
+ * Time: 9:10 AM
+ */
+
+namespace Viweb\SyliusProductBridgeBundle\EventListener;
+
+
+use Doctrine\Common\EventSubscriber;
+use Doctrine\ORM\Event\LoadClassMetadataEventArgs;
+use Doctrine\ORM\Events;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Viweb\SyliusProductBridgeBundle\Entity\Category;
+use Viweb\SystemBundle\Event\ConfigureFrontendMenuEvent;
+
+//use Doctrine\Common\Persistence\Mapping\ClassMetadata;
+
+class ProductMenuListener implements EventSubscriberInterface
+{
+
+    public static function getSubscribedEvents()
+    {
+        return array(
+            ConfigureFrontendMenuEvent::CONFIGURE =>
+                [
+                    ['processMenu', 0]
+                ]
+        );
+    }
+
+    public function processMenu(ConfigureFrontendMenuEvent $event)
+    {
+
+    }
+
+
+}
